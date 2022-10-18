bcb9bffbe11b81e132971c61df2a4b456fcc5a87
Sakshi Sharma <sakshi.sharma7595@gmail.com>
2022-10-19 00:33:06 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+    //HERO SLIDER
+    $('#hero-slider').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        items: 1,
+        dots: false,
+        smartSpeed: 1000, 
+        navText : ['PREV', 'NEXT'],
+        responsive:{
+            0:{
+                nav: false,
+            },
+            768:{
+                nav: true
+            },
+            
+        }
+    });
+
+
+    //PROJECT SLIDER
+    $('#project-slider').owlCarousel({
+        loop:true,
+        margin:0,
+        nav: true,
+        dots: false,
+        smartSpeed: 1000, 
+        margin: 24,
+        navText: ['PREV', 'NEXT'],
+        responsive:{
+            0:{
+                items: 1,
+                nav: false,
+                margin: 0,
+            },
+            768:{
+                items: 2,
+            },
+            1140: {
+                items: 2,
+                center: true, //properties
+                dots: true,
+            }
+            
+        }
+    })
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        dots: true,
+        items:1,
+        smartspeed: 800,
+    })
+});
+
